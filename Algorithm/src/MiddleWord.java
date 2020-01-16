da86014628d2a45c0c743f3b12d232bde7ae0aba
100Gyeon <58380158+100Gyeon@users.noreply.github.com>
2020-01-16 23:52:49 +0900

    Update MiddleWord.java

--- Algorithm/src/MiddleWord.java
+++ Algorithm/src/MiddleWord.java
@@ -1,4 +1,15 @@
 
-public class MiddleWord {
-
+class MiddleWord {
+  public String solution(String s) {
+      String answer = "";
+      int length = s.length();
+      if(length % 2 != 0){
+          answer = s.substring(length/2, length/2 + 1);
+          return answer;
+      }
+      else {
+          answer = s.substring(length/2 - 1, length/2 + 1);
+          return answer;
+      }
+  }
 }
