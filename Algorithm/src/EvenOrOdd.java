4f044c638bc0b56565cf88d7922e3155b4636f7a
100Gyeon <58380158+100Gyeon@users.noreply.github.com>
2020-01-17 23:45:32 +0900

    Level 1) 짝수와 홀수

---
+++ Algorithm/src/EvenOrOdd.java
@@ -0,0 +1,17 @@
+
+public class EvenOrOdd {
+	public String solution(int num) {
+	      String answer = "";
+	      if(num % 2 == 0) answer = "Even";
+	      else answer = "Odd";
+	      return answer;
+	  }
+}
+
+/* 삼항연산자로 코드를 간결하게 만들 수 있다.
+ * 
+ * public String solution(int num) {
+ * 		return num % 2 == 0 ? "Even" : "Odd";
+ * }
+ * 
+ */
