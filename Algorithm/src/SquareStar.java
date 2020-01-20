101ff9561017c9475ba81c1e84415aacacf43181
100Gyeon <58380158+100Gyeon@users.noreply.github.com>
2020-01-21 02:50:43 +0900

    Level 1) 직사각형 별찍기

--- Algorithm/src/SquareStar.java
+++ Algorithm/src/SquareStar.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 class SquareStar {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        	int a = sc.nextInt();
+        	int b = sc.nextInt();
 
-        for(int i=1; i<=b; i++){
-            for(int j=1; j<=a; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+		for(int i=1; i<=b; i++){
+		    for(int j=1; j<=a; j++){
+			System.out.print("*");
+		    }
+		    System.out.println();
+		}
         
-        sc.close();
+        	sc.close();
     }
 }
