958c0c152d5768ee705b065d09c24d5f0da0a807
100Gyeon <58380158+100Gyeon@users.noreply.github.com>
2020-01-26 00:54:18 +0900

    Level 1) 정수 내림차순으로 배치하기

--- Algorithm/src/DescendingOrder.java
+++ Algorithm/src/DescendingOrder.java
@@ -1,3 +1,8 @@
+// 오름차순 정렬 : Arrays.sort(배열 이름);
+// 내림차순 정렬 : Arrays.sort(배열 이름, Collections.reverseOrder());
+// String.split(구분자);
+// String.join(구분자, 배열); -> 배열을 하나의 String으로 바꿔주는 메소드
+
 import java.util.Arrays;
 import java.util.Collections;
 
