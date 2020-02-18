5162c2c3a645922d584a217cd02e877c630d3a33
100Gyeon <bjy9966@naver.com>
2020-02-18 12:43:26 +0900

    Level 1) 예산

---
+++ src/level1/Budget.java
@@ -0,0 +1,20 @@
+package level1;
+
+import java.util.Arrays;
+
+class Budget {
+	public int solution(int[] d, int budget) {
+		int answer = 0;
+		Arrays.sort(d); // 1. 배열 오름차순 정렬
+		for (int i = 0; i < d.length; i++) {
+			if (budget >= d[i]) {
+				budget -= d[i];
+				answer++; 
+				// 2. 예산이 신청 금액보다 크거나 같으면, 예산 - 신청 금액
+				// 예산과 신청 금액의 크기를 비교 -> 뺄셈 -> 카운트 증가
+			} else
+				break; // 3. 아니면 반복문 탈출
+		}
+		return answer;
+	}
+}
