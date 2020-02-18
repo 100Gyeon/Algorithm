d0365817114faf238e965166358541d54a37baef
100Gyeon <bjy9966@naver.com>
2020-02-18 12:56:45 +0900

    Level 1) 문자열 내림차순으로 배치하기

---
+++ src/level1/ReverseString.java
@@ -0,0 +1,14 @@
+package level1;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+class ReverseString {
+	public String solution(String s) {
+		String answer = "";
+		String[] str = s.split("");
+		Arrays.sort(str, Collections.reverseOrder());
+		answer = String.join("", str);
+		return answer;
+	}
+}
