b18e3bb66ed22189f5202b6eb3c0301b88d38f12
100Gyeon <bjy9966@naver.com>
2020-02-18 12:56:14 +0900

    Level 1) 제일 작은 수 제거하기

---
+++ src/level1/RemoveMin.java
@@ -0,0 +1,25 @@
+package level1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class RemoveMin {
+	public int[] solution(int[] arr) {
+		if (arr.length == 1) {
+			arr[0] = -1;
+			return arr;
+		}
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			al.add(arr[i]);
+		}
+		Integer minimum = Collections.min(al);
+		// Collections.min()으로 ArrayList의 최솟값을 얻는다.
+		al.remove(minimum);
+		int[] answer = new int[arr.length - 1];
+		for (int i = 0; i < al.size(); i++) {
+			answer[i] = al.get(i);
+		}
+		return answer;
+	}
+}
