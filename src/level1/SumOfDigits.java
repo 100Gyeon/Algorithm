403cd07f598a2ea605d3664b96472478b968e54f
100Gyeon <bjy9966@naver.com>
2020-02-18 12:58:30 +0900

    Level 1) 자릿수 더하기

---
+++ src/level1/SumOfDigits.java
@@ -0,0 +1,14 @@
+package level1;
+
+class SumOfDigits {
+	public int solution(int n) {
+		int answer = 0;
+		String s = Integer.toString(n);
+		StringBuffer sb = new StringBuffer(s);
+		String[] sArray = sb.toString().split("");
+		for (int i = 0; i < sArray.length; i++) {
+			answer += Integer.parseInt(sArray[i]);
+		}
+		return answer;
+	}
+}
