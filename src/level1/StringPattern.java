5d7e05887efe28138410bf803b3517433390787f
100Gyeon <bjy9966@naver.com>
2020-02-18 12:57:52 +0900

    Level 1) 수박수박수박수박수박수?

---
+++ src/level1/StringPattern.java
@@ -0,0 +1,14 @@
+package level1;
+
+class StringPattern {
+	public String solution(int n) {
+		String answer = "";
+		for (int i = 0; i < n; i++) {
+			if (i % 2 == 0)
+				answer = answer + "¼ö";
+			else
+				answer = answer + "¹Ú";
+		}
+		return answer;
+	}
+}
