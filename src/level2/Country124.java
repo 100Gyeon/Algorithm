9fe696294d2b1ef8af60cbee0fd0ce729c06d30f
100Gyeon <bjy9966@naver.com>
2020-02-20 18:00:40 +0900

    Level 2) 124 나라의 숫자

---
+++ src/level2/Country124.java
@@ -0,0 +1,13 @@
+package level2;
+
+class Country124 {
+	public String solution(int n) {
+		String answer = "";
+		String num[] = { "4", "1", "2" };
+		while (n > 0) {
+			answer = num[n % 3] + answer;
+			n = (n - 1) / 3;
+		}
+		return answer;
+	}
+}
