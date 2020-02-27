cdd4e10aef70a19806153ee179bc0431f47a9ada
100Gyeon <bjy9966@naver.com>
2020-02-28 01:19:04 +0900

    Level 2) H-Index

--- src/level2/H_index.java
+++ src/level2/H_index.java
@@ -2,14 +2,14 @@ package level2;
 
 import java.util.Arrays;
 
-public class H_index {
+class H_index {
 	public int solution(int[] citations) {
 		int answer = 0;
 		int length = citations.length;
 		Arrays.sort(citations);
-		for(int i=0 ; i<length ; i++) {
-			if(citations[i] <= length-i) {
-				answer = i;
+		for (int i = 0; i < length; i++) {
+			if (citations[i] >= length - i) {
+				answer = length - i;
 				break;
 			}
 		}
