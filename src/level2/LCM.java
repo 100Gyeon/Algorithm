490eb9d780d2f2f11c52350389d450461df2665b
100Gyeon <bjy9966@naver.com>
2020-07-19 22:15:37 +0900

    Level 2) N개의 최소공배수

---
+++ src/level2/LCM.java
@@ -0,0 +1,32 @@
+package level2;
+
+class LCM {
+
+	int euclid(int x, int y) { // 유클리드 호제법
+		int min = Math.min(x, y);
+		int max = Math.max(x, y);
+		int gcd, lcm;
+
+		while (true) {
+			int result = max % min;
+			if (result == 0) {
+				gcd = min; 		   // 최대공약수
+				lcm = x * y / gcd; // 최소공배수 = 두 수의 곱 / 최대공배수
+				break;
+			} else {
+				max = min;
+				min = result;
+			}
+		}
+		
+		return lcm;
+	}
+
+	public int solution(int[] arr) {
+		int answer = arr[0];
+		for (int i = 1; i < arr.length; i++) {
+			answer = euclid(answer, arr[i]);
+		}
+		return answer;
+	}
+}
